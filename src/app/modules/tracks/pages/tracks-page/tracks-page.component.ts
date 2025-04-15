28fa94e440215ac550a00cff0092ce1271519fc3
Kalio <kalio@Kalios-Mac-mini.local>
2025-04-14 19:32:02 -0600

    Intro to RxJS

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { SectionGenericComponent } from "../../../../shared/components/section-generic/section-generic.component";
-import * as dataRaw from '../../../../data/tracks.json';
-import { TrackModel } from '@core/models/tracks.models';
+
+import { TrackModel } from "@core/models/tracks.models";
+import { TrackService } from "@modules/tracks/services/track.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-tracks-page',
+  selector: "app-tracks-page",
   imports: [SectionGenericComponent],
-  templateUrl: './tracks-page.component.html',
-  styleUrl: './tracks-page.component.css'
+  templateUrl: "./tracks-page.component.html",
+  styleUrl: "./tracks-page.component.css",
 })
-export class TracksPageComponent implements OnInit{
-
-  mockTracksList: Array<TrackModel> = [
+export class TracksPageComponent implements OnInit {
   
-  ]
+  tracksTrending: Array<TrackModel> = [];
+  tracksRandom: Array<TrackModel> = [];
+
+  listObservers$: Array<Subscription> = [];
+
+  constructor(private trackService: TrackService) {}
 
   ngOnInit(): void {
-    const { data } : any = (dataRaw as any).default;
-    this.mockTracksList = data;
+    const observer1$ = this.trackService.dataTracksTrending$.subscribe(
+      (response) => {
+        this.tracksTrending = response;
+        console.log("canciones trending 👌 --->", response);
+      }
+    );
+
+    const observer2$ = this.trackService.dataTracksRandom$.subscribe(
+      (response) => {
+        this.tracksRandom = [...this.tracksRandom, ...response];
+        console.log("canciones random 👍 --->", response);
+      }
+    );
+
+    this.listObservers$ = [observer1$, observer2$];
   }
 
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(o => o.unsubscribe());
+  }
 }
