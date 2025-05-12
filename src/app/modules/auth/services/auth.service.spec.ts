d0000ffd4538da0ba70aab040ee2995211ed330e
Kalio <kalio@Kalios-Mac-mini.local>
2025-05-11 21:01:25 -0600

    Services test

--- src/app/modules/auth/services/auth.service.spec.ts
+++ src/app/modules/auth/services/auth.service.spec.ts
@@ -1,24 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { AuthService } from './auth.service';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthService } from "./auth.service";
+import { HttpClient, provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
 
-describe('AuthService', () => {
+import * as moockRaw from "../../../data/user.json";
+import { of } from "rxjs";
+
+describe("AuthService", () => {
   let service: AuthService;
+  let mockUser: any = (moockRaw as any).default;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting(),
-        AuthService
-      ]
+      providers: [provideHttpClient(), provideHttpClientTesting(), AuthService],
     });
-    service = TestBed.inject(AuthService);
+    httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['post']);
+    const routerSpy = jasmine.createSpyObj("Router", ["navigate"]);
+    const cookieSpy = jasmine.createSpyObj("CookieService", ["set"]);
+
+    service = new AuthService(httpClientSpy, cookieSpy, routerSpy);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  it("should return an object with 'data' and 'tokenSession'",(done: DoneFn) => {
+    
+    // Arrange
+    const user: any = mockUser.userOk;
+    const mockResponse = {
+      data: {},
+      tokenSession: 'token'
+    }
+
+    httpClientSpy.post.and.returnValue(
+      of(mockResponse)
+    );
+
+    // Act
+    service.sendCredential(user.email, user.password)
+    .subscribe(responseApi => {
+      const getProperties = Object.keys(responseApi);
+      expect(getProperties).toContain('data');
+      expect(getProperties).toContain('tokenSession');
+      done();
+    })
+  })
 });
--- src/app/shared/pipe/order-list.pipe.spec.ts
+++ src/app/shared/pipe/order-list.pipe.spec.ts
@@ -1,8 +1,39 @@
 import { OrderListPipe } from './order-list.pipe';
+import * as mockRaw from '../../data/tracks.json';
+import { TrackModel } from '@core/models/tracks.models';
 
 describe('OrderListPipe', () => {
   it('create an instance', () => {
     const pipe = new OrderListPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('testing data entry and output', () => {
+    // Arrange
+    const pipe = new OrderListPipe();
+    const { data }: any = (mockRaw as any).default;
+
+    // Act
+    const result: TrackModel[] = pipe.transform(data);
+
+    // Assert
+    expect(result).toEqual(data);
+  })
+
+  it('testing ascending order', () => {
+    // Arrange
+    const pipe = new OrderListPipe();
+    const { data }: any = (mockRaw as any).default;
+    const firstValue = data.find((i: any) => i._id === 7);
+    const lastValue = data.find((i: any) => i._id === 6);
+
+    // Act
+    const result: TrackModel[] = pipe.transform(data, 'name', 'asc');
+    const firstResult = result[0];
+    const lastResult = result[result.length-1];
+
+    // Assert
+    expect(firstResult).toEqual(firstValue);
+    expect(lastResult).toEqual(lastValue);
+  })
 });
